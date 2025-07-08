752d69cb43e311936e317c296cc33d0d88bb160c
Abhay Kishor <abhaykishor14@gmail.com>
2025-07-08 15:41:12 +0530

    initial commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://abhaykishor14:test123456@cluster0.bwbfhes.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGODB_URI).then(()=>console.log("DB Connected"));
 }
 
 
